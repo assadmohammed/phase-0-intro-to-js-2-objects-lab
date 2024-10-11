3011b3d79f082059c17fc14b4cd767fc5ee10936
assa mohammed <mohammedassad749@gmail.com>
2024-10-11 16:34:13 +0300

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "12th Junction Ngong"
+}
+//Nondestructive modification
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
+console.log(updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway'));
+console.log(employee);
+//destructuvely modify
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway'));
+//deleting property nondestructively
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj2 = {...obj};
+    delete newObj2[key];
+    return newObj2
+}
+let newEmployee = deleteFromEmployeeByKey(employee, 'name');
+console.log(newEmployee);
+console.log(employee);
+console.log(newEmployee['name']);
+console.log(typeof newEmployee);
+//destructive delete
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return employee
+}
+let newEmployee2 = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+console.log(newEmployee2['name']);
+console.log(newEmployee2);
+console.log(employee);
+console.log();
+
